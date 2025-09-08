a595894febba82327ec10713c8cb660c47af4197
djonkarter-star <djonkarter@gmail.com>
2025-09-08 22:46:45 +0300

    Add files via upload

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,7 @@ module.exports = {
   apps: [
     {
       name: "telegram-bot",
-      script: "server/src/index.js",
-      env: {
-        BOT_TOKEN: "6421391676:AAE-0C21BHQ3SVm0fysCHsQTuJRkDSOaSnk"
-      }
+      script: "server/src/index.js"
     }
   ]
 };
