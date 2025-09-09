bc87a866fa495992bbf11d56140f1af620fafabc
djonkarter-star <djonkarter@gmail.com>
2025-09-09 11:03:07 +0300

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,11 @@ module.exports = {
   apps: [
     {
       name: "telegram-bot",
-      script: "server/src/index.js"
+      script: "server/src/index.js",
+      env: {
+        BOT_TOKEN: "твой_токен_от_BotFather",
+        PORT: 3000
+      }
     }
   ]
-};
+}
