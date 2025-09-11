650b5ef0509c7ae3518224b6bfe729fc60a31888
djonkarter-star <djonkarter@gmail.com>
2025-09-11 20:29:28 +0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,14 @@
-// ecosystem.config.js
-const path = require("path");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
-
 module.exports = {
   apps: [
     {
       name: "telegram-bot",
-      script: "./server/src/index.js",
-      watch: false,
+      script: "index.js",
+      watch: true,
+      env_file: ".env",   // ✅ вот эта строка подхватит твой .env
       env: {
         NODE_ENV: "production",
-        PORT: process.env.PORT || 3000,
-        BOT_TOKEN: process.env.BOT_TOKEN, // явно подхватываем токен
-      },
-    },
-  ],
+        PORT: 3000
+      }
+    }
+  ]
 };
